ccdeb548b2d8015ee0d09fb756ef586cecc97161
MrQuentinet <quentinvilluis@gmail.com>
2023-08-23 00:49:15 -0400

    Add Jinja support and test

--- tests/texutils_test.py
+++ tests/texutils_test.py
@@ -5,22 +5,36 @@ import os
 from texutils.utils import compare_pdfs
 from texutils import TeXUtils
 
+parent_directory = os.path.dirname(os.path.abspath(__file__))
+tmp_folder = os.path.join(parent_directory, "tmp")
+sample_folder = os.path.join(parent_directory, "sample")
+
 
 @pytest.fixture(autouse=True)
 def setup_resources():
     print("Before test")
-    os.mkdir("./tests/tmp")
-    shutil.copy('./tests/sample/sample.tex', './tests/tmp/')
-    shutil.copy('./tests/sample/sample.pdf', './tests/tmp/')
+
+    os.makedirs(tmp_folder) if not os.path.exists(tmp_folder) else None
+
+    shutil.copy(os.path.join(sample_folder, "sample.tex"), tmp_folder)
+    shutil.copy(os.path.join(sample_folder, "sample.tex.j2"), tmp_folder)
 
     yield
 
     print("After test")
-    shutil.rmtree("./tests/tmp")
+    shutil.rmtree(tmp_folder)
 
 
 def test_tex_utils_from_file():
-    tex = TeXUtils.from_tex_file('./tests/tmp/sample.tex')
+    tex = TeXUtils.from_tex_file(os.path.join(tmp_folder, "sample.tex"))
     pdf = tex.create_pdf()
 
-    assert compare_pdfs('./tests/sample/sample.pdf', './tests/tmp/sample.pdf')
+    assert compare_pdfs(os.path.join(sample_folder, "sample.pdf"), os.path.join(tmp_folder, "sample.pdf"))
+
+
+def test_tex_utils_from_jinja_file():
+    data = {"name": "World"}
+    tex = TeXUtils.from_jinja_template_file(os.path.join(tmp_folder, "sample.tex.j2"), **data)
+    pdf = tex.create_pdf()
+
+    assert compare_pdfs(os.path.join(sample_folder, "sample.pdf"), os.path.join(tmp_folder, "sample.pdf"))
--- texutils/texutils.py
+++ texutils/texutils.py
@@ -1,7 +1,5 @@
-import tempfile
 import os
 import subprocess
-
 import jinja2
 
 
@@ -25,24 +23,25 @@ class TeXUtils:
         return cls(binstr, jobname, dir_name)
 
     @classmethod
-    def from_jinja_template_file(cls, filename, **render_kwargs):
+    def from_jinja_template_file(cls, filename, **data: any):
         dir_name = os.path.dirname(filename)
         prefix = os.path.basename(filename)
-        prefix = os.path.splitext(prefix)[0]
+        prefix = os.path.splitext(prefix)[0] if not prefix.endswith('.j2') else \
+        os.path.splitext(os.path.splitext(prefix)[0])[0]
 
         env = jinja2.Environment(loader=jinja2.FileSystemLoader(dir_name), autoescape=jinja2.select_autoescape())
 
-        template = env.get_template(filename)
-        cls.from_jinja_template(template, prefix, dir_name, **render_kwargs)
+        template = env.get_template(os.path.basename(filename))
+        return cls.from_jinja_template(template, prefix, dir_name, **data)
 
     @classmethod
-    def from_jinja_template(cls, template: jinja2.Template, jobname: str, dir_name: str = None, **render_kwargs):
-        tex_src = template.render(**render_kwargs)
-        return cls(tex_src, jobname, dir_name)
+    def from_jinja_template(cls, template: jinja2.Template, jobname: str, dir_name: str = None, **data):
+        tex_src = template.render(**data)
+        return cls(tex_src.encode("utf-8"), jobname, dir_name)
 
     def create_pdf(self):
         args = self.get_args()
         subprocess.run(['pdflatex', *args], input=self.tex)
 
     def get_args(self):
-        return [k+('='+v if v is not None else '') for k, v in self.params.items()]
+        return [k + ('=' + v if v is not None else '') for k, v in self.params.items()]
